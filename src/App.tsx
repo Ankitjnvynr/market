20a53eee712990bffe3a4a4d55a79639f2d759ad
pankajvijaybhatnagar <pankajbhatnagar98@gmail.com>
2026-05-04 12:24:38 +0530

    refactor: update App component to use TypeScript and improve imports feat: add OptionChainFilters and OptionChainTable components for enhanced functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-<<<<<<< HEAD
-import Hero from './components/Hero'
-=======
 import Hero from './components/Hero.tsx'
 
->>>>>>> four
 import './App.css'
 
 function App() {
+
+
   return (
     <>
       <Hero />
+
     </>
   )
 }
